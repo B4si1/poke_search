320e63aabd0ec75a94cec2252e4890a55f690e0a
B4si1 <153111672+B4si1@users.noreply.github.com>
2024-04-20 17:58:03 +0100

    Poke Search V2

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const inputString = document.getElementById("search-input");
 const inputBtn = document.getElementById("search-button");
+const inputRandomBtn = document.getElementById("random-button");
 const poke_name = document.getElementById("pokemon-name");
 const poke_id = document.getElementById("pokemon-id");
 const poke_hp = document.getElementById("hp");
@@ -14,41 +15,48 @@ const poke_special_defense = document.getElementById("special-defense");
 const poke_speed = document.getElementById("speed");
 const poke_types = document.getElementById("types");
 const poke_abilities = document.getElementById("abilities");
+const poke_flavour = document.getElementById("flavor");
 const poke_cries = document.getElementById("cries");
 const poke_warning = document.getElementById("warning");
 const error_display = document.getElementById("error");
 const titles = document.getElementById("titles");
+const poke_card = document.getElementById("poke-infos");
 
+inputRandomBtn.addEventListener('click', function(e){
+  clearCurrentSearch();
+  const input_string = Math.floor(Math.random() * 1025).toString();
+  if(input_string == ''){
+    error_display.innerHTML = 'Please enter a Pokémon name or ID!';
+  }else{
+    displayPokemonInfo(input_string);
+    
+  }
+})
 
 inputBtn.addEventListener('click', function(e){
   clearCurrentSearch();
-  if(inputString.value == ''){
+  const input_string = inputString.value;
+  if(input_string == ''){
     error_display.innerHTML = 'Please enter a Pokémon name or ID!';
   }else{
-    getPokemonInfo(inputString.value.toLowerCase());
+    displayPokemonInfo(input_string);
+    
   }
   
 });
 
-
-function getPokemonInfo(request) {
-  
-  const apiUrl = `https://pokeapi.co/api/v2/pokemon/${request}`;
-  
-  fetch(apiUrl)
-    .then(response => {
+async function fetchData(apiUrl) {
+  try {
+      const response = await fetch(apiUrl);
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+          throw new Error('Network response was not ok');
       }
-      return response.json();
-    })
-    .then(data => {
-      displayPokemonInfo(data);
-      })
-    .catch(error => {
-      error_display.innerHTML = "Pokémon not found!";
-  });
-
+      const data = await response.json();
+      return data;
+  } catch (error) {
+      error_display.innerHTML = 'Pokémon not found!';
+      return null; 
+  }
 }
 
 const ELEMENT_COLORS = [
@@ -72,6 +80,28 @@ const ELEMENT_COLORS = [
   {name: 'FAIRY', color: '#ee99ee',},
 ]
 
+const CLASS_COLORS = [
+  {color: 'RED', background:'#e87070', accent:'red',},
+  {color: 'BLUE', background:'#64c0bb', accent:'blue',},
+  {color: 'YELLOW', background:'#f8ca2b', accent:'yellow',},
+  {color: 'GREEN', background:'#309c00', accent:'green',},
+  {color: 'BLACK', background:'#575555', accent:'black',},
+  {color: 'BROWN', background:'#d5b38b', accent:'brown',},
+  {color: 'PURPLE', background:'#d181e0', accent:'purple',},
+  {color: 'GRAY', background:'#90908f', accent:'gray',},
+  {color: 'WHITE', background:'#b9b9b7', accent:'white',},
+  {color: 'PINK', background:'#fdccfd', accent: 'pink',},
+]
+
+function colorCard(colorName){
+  CLASS_COLORS.forEach(el => {
+    if(colorName === el.color){
+      poke_card.style = `background-color:${el.background};border-left: 10px solid ${el.accent};`
+    }
+  })
+
+}
+
 function elementHighlight(input){
   
   let colorValue = '';
@@ -84,9 +114,18 @@ function elementHighlight(input){
   return colorValue;
 }
 
-function displayPokemonInfo(info){
+async function displayPokemonInfo(input){
+
+  const apiUrl = input; 
+  const info = await fetchData('https://pokeapi.co/api/v2/pokemon/' + input);
+  const pokeSpeciesData = await fetchData('https://pokeapi.co/api/v2/pokemon-species/' + input); 
   const poke_data = info.types;
   const poke_moves = info.abilities;
+  
+  poke_card.classList.add('poke-infos-display')
+
+  
+
   poke_types.innerHTML = `TYPE(s) : `
 
   poke_data.forEach(element => {
@@ -94,7 +133,7 @@ function displayPokemonInfo(info){
   });
 
   poke_moves.forEach(element => {
-    poke_abilities.innerHTML += `<span>${element.ability.name.toUpperCase()} `;
+    poke_abilities.innerHTML += `<span><small>${element.ability.name.toUpperCase()}</small> `;
   })
 
   poke_name.innerHTML = `NAME : <span>${info.name.toUpperCase()}</span>`;
@@ -110,17 +149,21 @@ function displayPokemonInfo(info){
   poke_special_attack.innerHTML = `Special Attack : <span>${info.stats[3].base_stat}</span>`;
   poke_special_defense.innerHTML = `Special Defense : <span>${info.stats[4].base_stat}</span>`;
   poke_speed.innerHTML = `Speed : <span>${info.stats[5].base_stat}</span>`;
+  poke_flavour.innerHTML = `<span></span><small class='wrap'>${pokeSpeciesData.flavor_text_entries[0].flavor_text}</small><span></span>`;
   poke_cries.innerHTML += `<audio controls volume="0.2">
   <source src="${info.cries.latest}" type="audio/ogg">
   Your browser does not support this audio element.
   </audio>`
   poke_warning.innerHTML = `<span></span>volume warning!<span></span>`;
+
+  colorCard(pokeSpeciesData.color.name.toUpperCase())
 }
 
-const elements = [poke_types, poke_name, poke_hp, poke_id, poke_exp, poke_weight, poke_height, poke_attack, poke_defense, poke_special_attack, poke_special_defense, poke_speed, error, titles, poke_abilities, poke_warning, poke_cries];
+const elements = [poke_flavour, poke_types, poke_name, poke_hp, poke_id, poke_exp, poke_weight, poke_height, poke_attack, poke_defense, poke_special_attack, poke_special_defense, poke_speed, error, titles, poke_abilities, poke_warning, poke_cries];
 
 function clearCurrentSearch(){
-  
+  poke_card.classList.toggle('poke-infos-display');
+  poke_card.style = "";
   poke_img.src = "Poke.png";
   elements.forEach(el => {
     el.innerHTML = "";
